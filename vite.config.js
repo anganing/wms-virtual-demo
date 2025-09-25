214c25a10dfb0ba84ca1bea6f2fbba434eb11635
anganing <63218247+anganing@users.noreply.github.com>
2025-09-25 14:58:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  # base: '/wms-virtual-demo/', // 适配 GitHub Pages 仓库路径
+  base: '/wms-virtual-demo/', // 适配 GitHub Pages 仓库路径
   plugins: [vue()]
 }) 
